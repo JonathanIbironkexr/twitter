2aa97e7506481f0f3fe2d8c516932e7549c898e7
user142219 <user142219@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-12-05 22:12:21 -0500

    second commit

---
+++ Desktop/MovieFlixApp/MovieFlix/ViewControllers/NowPlayingViewController.swift
@@ -0,0 +1,82 @@
+//
+//  NowPlayingViewController.swift
+//  MovieFlix
+//
+//  Created by Trustin Harris on 2/3/18.
+//  Copyright © 2018 Trustin Harris. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class NowPlayingViewController: UIViewController,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+   var movies: [Movie] = []
+    
+    var refreshControl: UIRefreshControl!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        tableView.dataSource = self
+        fetchMovies()
+    }
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
+        fetchMovies()
+    }
+    
+    func fetchMovies(){
+        
+        MovieApiManager().nowPlayingMovies { (movies: [Movie]?, error: Error?) in
+            if let movies = movies {
+                self.movies = movies
+                self.tableView.reloadData()
+            }
+        }
+                self.tableView.reloadData()
+                self.activityIndicator.stopAnimating()
+                self.refreshControl.endRefreshing()
+                
+                
+   
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        cell.movie = movies[indexPath.row]
+
+        
+        self.tableView.rowHeight = 200
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cell = sender as! UITableViewCell
+        if let indexPath = tableView.indexPath(for: cell){
+            let movie = movies[indexPath.row]
+            let detailViewController = segue.destination as! DetailViewController
+            //detailViewController.movie = movie
+        }
+    }
+        
+        
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+        }
+}
